1f68d93b9d23e2b88b2da3dd8cef55242ff268ec
Sushma92 <nuthanap1992@gmail.com>
2019-11-23 17:09:39 -0600

    connected with h2 db

--- todo-app-main/todo-common/src/main/java/com/todo/common/entity/BaseEntity.java
+++ todo-app-main/todo-common/src/main/java/com/todo/common/entity/BaseEntity.java
@@ -1,14 +1,23 @@
 package com.todo.common.entity;
 
-public abstract class BaseEntity {
-	private int ID;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long ID;
 	private String Status;
 
-	public int getID() {
+	public Long getID() {
 		return ID;
 	}
 
-	public void setID(int iD) {
+	public void setID(Long iD) {
 		ID = iD;
 	}
 
