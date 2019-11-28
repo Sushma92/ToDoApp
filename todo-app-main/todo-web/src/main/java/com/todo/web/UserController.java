e1aa8c424350643d6b75b2ca16cb5b7537f0b58b
Sushma92 <nuthanap1992@gmail.com>
2019-11-28 16:13:26 -0600

    added cors

--- todo-app-main/todo-web/src/main/java/com/todo/web/UserController.java
+++ todo-app-main/todo-web/src/main/java/com/todo/web/UserController.java
@@ -1,6 +1,7 @@
 package com.todo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import com.todo.services.BaseService;
 import com.todo.services.UserServiceImpl;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "/api/services/todo-app/user")
 
 public class UserController extends BaseController<User, User>{
